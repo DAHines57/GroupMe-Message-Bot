fa472b1d94ecd498d8da7aa681feb2eb02a1bed4
Boijangle <hitechdill@gmail.com>
2017-05-13 21:52:28 -0500

    Actually works in Groupme
    The bot can take a message and parse it accordingly

--- test.py
+++ test.py
@@ -3,6 +3,7 @@ import requests
 from os.path import join, dirname
 from dotenv import load_dotenv
 from flask import Flask
+from flask import request
 app = Flask(__name__)
 
 dotenv_path = join(dirname(__file__), '.env')
@@ -10,7 +11,16 @@ load_dotenv(dotenv_path)
 
 bot_id = os.environ.get("BOT_ID")
 
-@app.route('/post/<uText>')
-def show_user_profile(uText):
-    r = requests.post('https://api.groupme.com/v3/bots/post', params = {'bot_id' : bot_id, 'text' : uText})
-    return 'Post: %s' % uText
+def post_Text(user_Text):
+    requests.post('https://api.groupme.com/v3/bots/post', params = {'bot_id' : bot_id, 'text' : user_Text})
+
+@app.route('/callback', methods=['POST'])
+def parse_messages():
+    message = request.get_json()
+    if message['sender_type'] != "user":
+        return 'OK'
+    if "bupd" in message['text'].lower():
+        post_Text("JOHNNY LAW")
+    if "Hi" in message['text'].lower():
+        post_Text("Hi " + message['name'].split(" ")[0] + "!")
+    return 'OK'
