2ab6177f61a5ec35456ab11c94901608d4ea58ef
Boijangle <hitechdill@gmail.com>
2017-05-14 19:27:09 -0500

    More exception fixing

--- test.py
+++ test.py
@@ -17,28 +17,27 @@ def post_text(user_text, bot_id):
 
 @app.route('/callback/<bot_id>', methods=['POST'])
 def parse_messages(bot_id):
-    message = request.get_json()
-    if message['sender_type'] != "user":
+    try:
+        message = request.get_json()
+        if message['sender_type'] != "user":
+            return 'OK'
+
+        # BUPD Things
+        if request.args.get('bupd', 'off') != 'off':
+            if "bupd" in message['text'].lower():
+                post_text(bupd_responses[request.args.get('bupd','')], bot_id)
+
+        # Say hello to anyone that says "Hi"
+        if "Hi" in message['text']:
+            x=5/0
+            post_text("Hi " + message['name'].split(" ")[0] + "!", bot_id)
+
+        """
+        # Get annoyed at long texts
+        if len(message['text']) >= 300:
+            post_text("Cool story bro.")
+        """
         return 'OK'
-
-    # BUPD Things
-    if request.args.get('bupd', 'off') != 'off':
-        if "bupd" in message['text'].lower():
-            post_text(bupd_responses[request.args.get('bupd','')], bot_id)
-
-    # Say hello to anyone that says "Hi"
-    if "Hi" in message['text']:
-        x=5/0
-        post_text("Hi " + message['name'].split(" ")[0] + "!", bot_id)
-
-    """
-    # Get annoyed at long texts
-    if len(message['text']) >= 300:
-        post_text("Cool story bro.")
-    """
-    return 'OK'
-
-@app.errorhandler(werkzeug.exceptions.InternalServerError)
-def handle_error(e):
-    post_text(u'\U0001F916\U0001F915: ' + str(e), bot_id)
-    return 'Not OK'
+    except Exception as e:
+            post_text(u'\U0001F916\U0001F915: ' + str(e), bot_id)
+            return 'Not OK'
