7b0f97bcf13d2ef1180f9b376fc0fbd85b64e8b6
Boijangle <hitechdill@gmail.com>
2017-05-27 23:29:21 -0500

    Added saddness and sorrow text

--- test.py
+++ test.py
@@ -2,6 +2,7 @@ import os
 import requests
 import werkzeug
 import giphypop
+import random
 from os.path import join, dirname
 from dotenv import load_dotenv
 from flask import Flask
@@ -12,6 +13,7 @@ dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
 bupd_responses = {'jlaw': 'JOHNNY LAW'}
+sadness_texts = [line.strip() for line in open('list of saddness.txt')]
 gif = giphypop.Giphy()
 
 def post_text(user_text, bot_id):
@@ -37,6 +39,10 @@ def parse_messages(bot_id):
         if message['text'].startswith("/gif"):
             gif_search = message['text'][5:]
             post_text(gif.translate(gif_search).media_url, bot_id)
+
+        # Everything in life is suffering and sorrow
+        if "sad" in message['text'].lower():
+            post_text(random.choice(sadness_texts), bot_id)
 
         """
         # Get annoyed at long texts
