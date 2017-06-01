a03d1c1f151ca8801dfc29c4d469ea3f8b4a3131
Boijangle <hitechdill@gmail.com>
2017-05-31 20:14:48 -0500

    Frickle Frackle error messages

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import os
 import giphypop
 import random
+import subprocess
 from libs import post_text
 from os.path import join, dirname
 from dotenv import load_dotenv
@@ -15,7 +16,11 @@ bupd_responses = {'jlaw': 'JOHNNY LAW'}
 gif = giphypop.Giphy()
 
 if os.environ.get('BOOT_GROUP') is not None:
-    post_text(u'\U0001F916\U0001F305 Reporting for duty!', os.environ.get('BOOT_GROUP'))
+    print("FRICKLE FRACKLE")
+    last_commit = subprocess.run('git log -1 --pretty=format:%s', stdout=subprocess.PIPE).stdout
+    post_text(u'\U0001F916\U0001F305 Reporting for duty! Last update: ' + str(last_commit, encoding='utf-8'), os.environ.get('BOOT_GROUP'))
+else
+    print("FRICKLE FRACKLE")
 
 @app.route('/callback/<bot_id>', methods=['POST'])
 def parse_messages(bot_id):
