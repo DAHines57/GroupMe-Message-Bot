1b69566c47cbf976af3457e31ba040be6e6b9e82
Boijangle <hitechdill@gmail.com>
2017-07-26 21:33:27 -0500

    My regex is bad I feel bad

--- test.py
+++ test.py
@@ -45,7 +45,7 @@ def parse_messages(bot_id):
 
         # Post gif from Giphy
         if message['text'].startswith("/gif"):
-            search = re.search(r"/gif (.*?)( \d+)$?", message['text'])
+            search = re.search(r"/gif (.*?)( \d+)?$", message['text'])
             (gif_search, num) = search.groups('1')
             for i in range(int(num)):
                 post_text(gif.translate(gif_search).media_url, bot_id)
