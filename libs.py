1ebf76e74f348382a6c17b1b54c2736cd8935c75
Matt Horn <mathphreak@gmail.com>
2017-09-11 17:04:21 -0500

    Check for empty message

--- libs.py
+++ libs.py
@@ -1,4 +1,6 @@
 import requests
 
 def post_text(user_text, bot_id):
+    if len(user_text.strip()) == 0:
+        raise ValueError("Can't post empty message")
     requests.post('https://api.groupme.com/v3/bots/post', params = {'bot_id' : bot_id, 'text' : user_text}).raise_for_status()
