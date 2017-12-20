fcb50076792b74944f6719ec7ea5cac2e9bc6d42
Boijangle <hitechdill@gmail.com>
2017-12-20 09:06:55 -0600

    I am idiot

--- spotify.py
+++ spotify.py
@@ -29,7 +29,7 @@ while tracks['next']:
     all_tracks += tracks['items']
 
 random_track = random.choice(all_tracks)
-if(datetime.datetime.today().weekday() == 0):
+if(datetime.today().weekday() == 0):
     post_text(u'\U0001F3B5\U0001F4C5: ' + random_track['track']['name'] + ' - ' + random_track['track']['artists'][0]['name'] + ' ' + random_track['track']['external_urls']['spotify'], sys.argv[1])
 else:
-    post_text(datetime.datetime.today().weekday(), sys.argv[1])
+    post_text(datetime.today().weekday(), sys.argv[1])
