41c467ec6b0564e7670050033dcf6f49c6a6097d
Dylan Hines <hitechdill@gmail.com>
2018-06-06 11:07:35 -0500

    Debug points

--- database.py
+++ database.py
@@ -16,6 +16,7 @@ last_msg = Table('last_msg', meta, autoload=True, autoload_with=engine, schema='
 
 def store_last_msg(groupId, msgId, msgText):
      print("Select")
+     print(last_msg)
      s=select([last_msg]).where(last_msg.c.group_id == groupId)
      result = conn.execute(s)
      row = result.fetchall()
