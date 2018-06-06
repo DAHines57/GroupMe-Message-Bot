470c2e0ccab9aa5cada1b8d4e3c467a5f11511bf
Dylan Hines <hitechdill@gmail.com>
2018-06-05 19:44:18 -0500

    INDENT

--- test.py
+++ test.py
@@ -28,7 +28,7 @@ def parse_messages(bot_id):
         message = request.get_json()
 
         if message['sender_type'] != "user":
-        store_last_msg(message['group_id'], message['id'], message['text'])
+            store_last_msg(message['group_id'], message['id'], message['text'])
             return 'OK'
 
         """ Group Specific Actions """
