d062a211cf0e5860aa0d2997bb415a207fb4241b
Dylan Hines <hitechdill@gmail.com>
2018-06-09 14:54:08 -0500

    Bold didnt work

--- help.py
+++ help.py
@@ -1,9 +1,9 @@
-help_text = """\033[1mI'm a GroupMe bot that can do all sorts of things!
+help_text = """I'm a GroupMe bot that can do all sorts of things!
 /help - Displays this help text
 /gif <text> <num>* - Find <num> gifs of <text>
 /clap <text> - Clap after every word in <text>
 /durr - MoCk SoMeOnE's LaSt Message
 /joke - I'll tell a terrible joke.
 
-I also do stuff on my own...but that's a surprise\033[0m.
+I also do stuff on my own...but that's a surprise.
 """
