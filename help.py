a353691a698edb1b190a9a0f973ef0f72fe7c162
Dylan Hines <hitechdill@gmail.com>
2018-06-09 15:23:06 -0500

    Fix help text

--- help.py
+++ help.py
@@ -2,8 +2,9 @@ help_text = """I'm a GroupMe bot that can do all sorts of things!
 /help - Displays this help text
 /gif <text> <num>* - Find <num> gifs of <text>
 /clap <text> - Clap after every word in <text>
-/durr - MoCk SoMeOnE's LaSt Message
+/durr - MoCk SoMeOnE's LaSt MeSsAgE
 /joke - I'll tell a terrible joke.
 
 I also do stuff on my own...but that's a surprise.
+Made by Dylan Hines.
 """
