3fdea80a4f949678ff2460a4f0edf002402a8cf9
Dylan Hines <hitechdill@gmail.com>
2018-06-09 16:03:39 -0500

    @ppl pls work

--- test.py
+++ test.py
@@ -60,7 +60,7 @@ def parse_messages(bot_id):
 
         #Test posting
         if message['text'].startswith("/test") and message['sender_id'] == admin_sender_id:
-            post_text_mention("hiiiiiiiiiiiiii", bot_id, "Dylan Hines")
+            post_text_mention("@Dylan Hines", bot_id, "Dylan Hines")
 
 
         """ Actions for all groups """
