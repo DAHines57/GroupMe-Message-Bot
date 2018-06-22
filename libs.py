a60eee89b6e5a74ac0cfc24f2a00a45351c8ae5c
Dylan Hines <hitechdill@gmail.com>
2018-06-22 18:26:07 -0500

    LET ME KNOW IF THE BOT IS SILENCED

--- libs.py
+++ libs.py
@@ -8,6 +8,8 @@ def post_text(user_text, bot_id):
         raise ValueError("Can't post empty message")
     if(not check_silenced(bot_id)[0]):
         requests.post('https://api.groupme.com/v3/bots/post', params = {'bot_id' : bot_id, 'text' : user_text}).raise_for_status()
+    else:
+        print("BOT IS SILENCED")
 
 def post_text_mention(user_text, bot_id, mention_id):
     time.sleep(1)
@@ -20,5 +22,8 @@ def post_text_mention(user_text, bot_id, mention_id):
       'bot_id': bot_id,
       'attachments': [{ 'loci': [[0,0]], 'type': "mentions", 'user_ids': [mention_id] }]
     };
-
-    requests.post('https://api.groupme.com/v3/bots/post', json=payload).raise_for_status()
+    
+    if(not check_silenced(bot_id)[0]):
+        requests.post('https://api.groupme.com/v3/bots/post', json=payload).raise_for_status()
+    else:
+        print("BOT IS SILENCED")
