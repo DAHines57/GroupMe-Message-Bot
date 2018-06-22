4b02262edb8cd323f22d11aa056a654ebd9dbb35
Dylan Hines <hitechdill@gmail.com>
2018-06-22 14:49:20 -0500

    Testing to request bin

--- libs.py
+++ libs.py
@@ -15,5 +15,5 @@ def post_text_mention(user_text, bot_id, user_name):
         raise ValueError("Can't post empty message")
     mention_id = get_user_id(user_name)[0]
     print("User id: " + mention_id)
-    requests.post('https://api.groupme.com/v3/bots/post', params = {'attachments' : [{'loci':[[0,12]],'type':'mentions','user_ids':[mention_id]}]
+    requests.post('http://requestbin.fullcontact.com/y73zxty7', params = {'attachments' : [{'loci':[[0,12]],'type':'mentions','user_ids':[mention_id]}]
     ,'bot_id' : bot_id, 'text' : user_text}).raise_for_status()
