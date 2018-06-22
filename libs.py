1840638d390defeab50aa9d301f27b0b117a1a40
Dylan Hines <hitechdill@gmail.com>
2018-06-22 17:43:32 -0500

    Doin Sum Testin

--- libs.py
+++ libs.py
@@ -19,7 +19,7 @@ def post_text_mention(user_text, bot_id, user_name):
     payload = {
       'text': user_text,
       'bot_id': bot_id,
-      'attachments': [{ 'loci': [[0,12]], 'type': "mentions", 'user_ids': [mention_id] }]
+      'attachments': [{ 'loci': [[0,1]], 'type': "mentions", 'user_ids': [mention_id] }]
     };
 
     requests.post('https://api.groupme.com/v3/bots/post', json=payload).raise_for_status()
