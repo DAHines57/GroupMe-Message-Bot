8590ecd1d7580c32e0269e79ce19f7b1f78adc32
Dylan Hines <hitechdill@gmail.com>
2018-06-25 02:52:51 -0500

    Hardcode ids

--- libs.py
+++ libs.py
@@ -27,7 +27,7 @@ def post_text_mention(user_text, bot_id, mention_ids):
     payload = {
       'text': user_text,
       'bot_id': bot_id,
-      'attachments': [{ 'loci': [[0,0]], 'type': "mentions", 'user_ids': [mentions] }]
+      'attachments': [{ 'loci': [[0,0]], 'type': "mentions", 'user_ids': ["6567358","19791433"] }]
     };
 
     if(not check_silenced(bot_id)[0]):
