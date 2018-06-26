ff213342abbe94828310bf22522be315f5f3ed35
Dylan Hines <hitechdill@gmail.com>
2018-06-25 19:09:38 -0500

    Fixed stupid compliments code

--- compliments.py
+++ compliments.py
@@ -4,7 +4,7 @@ def generate_compliment():
 
     #part1 of the sentence---noun/subject
     part1 = [
-        "You",
+        "you",
     ]
 
     #part2 of the sentence--the verb/action
@@ -14,49 +14,50 @@ def generate_compliment():
 
     #part3 of the sentence--the ending(noun)
     part3 = [
-        "Extremely intuitive",
-        "A searcher of hidden meanings",
-        "Sensitive and perceptive",
-        "Gifted at reading others",
-        "A holder of strong convictions and beliefs",
-        "A person who will not compromise their ideals",
-        "Genuinely warm and affirming by nature",
-        "Capable of trusting your own instincts (and with good reason)",
-        "Usually right and you usually know it",
-        "Typically gentle and caring",
-        "Usually have good communication skills",
-        "A gifted writer",
-        "Committed and you take commitment seriously",
-        "The reason why I admire you deeply",
-        "A seeker of lifelong relationships",
-        "A good listener",
-        "Deep, complex and intense",
-        "Artistic and creative",
-        "A inspirator, a motivator, an achiever",
-        "Extremely insightful about people and situations",
+        "extremely intuitive",
+        "a searcher of hidden meanings",
+        "sensitive and perceptive",
+        "gifted at reading others",
+        "a holder of strong convictions and beliefs",
+        "a person who will not compromise their ideals",
+        "genuinely warm and affirming by nature",
+        "capable of trusting your own instincts (and with good reason)",
+        "usually right and you usually know it",
+        "typically gentle and caring",
+        "usually have good communication skills",
+        "a gifted writer",
+        "committed and you take commitment seriously",
+        "the reason why I admire you deeply",
+        "a seeker of lifelong relationships",
+        "a good listener",
+        "deep, complex and intense",
+        "artistic and creative",
+        "a inspirator, a motivator, an achiever",
+        "extremely insightful about people and situations",
         "a Perfectionist",
-        "Natural nurturer",
-        "Devoted to and protective of those they care about",
-        "The rarest of all types",
-        "An independent worker",
-        "In some ways, be easy-going",
-        "Everything I want to be",
-        "Very important to me",
-        "Not a failure",
-        "Special and idiosyncratic (heh, in a good way)",
-        "Beautiful, not just outside, but deep inside as well",
-        "The person I want to risk myself for because God risk Himself on me",
-        "Perfection, even the sun is jealous of the way you shine",
-        "So kind to others",
-        "A believer that there is good in this world",
-        "Constantly racing through my mind",
-        "The next Victoria Secret Model",
-        "Loved always",
-        "Pretty, witty, and gracious",
-        "Important",
-        "Geniune and sincere",
-        "One I went to spend my time with, even the future",
-        "Hotter than donut grease",
+        "natural nurturer",
+        "devoted to and protective of those they care about",
+        "the rarest of all types",
+        "an independent worker",
+        "in some ways, be easy-going",
+        "everything I want to be",
+        "very important to me",
+        "not a failure",
+        "special and idiosyncratic (heh, in a good way)",
+        "beautiful, not just outside, but deep inside as well",
+        "the person I want to risk myself for because God risk Himself on me",
+        "perfection, even the sun is jealous of the way you shine",
+        "so kind to others",
+        "a believer that there is good in this world",
+        "constantly racing through my mind",
+        "the next Victoria Secret Model",
+        "loved always",
+        "pretty, witty, and gracious",
+        "important",
+        "geniune and sincere",
+        "one I went to spend my time with, even the future",
+        "hotter than donut grease",
+        "a snack",
 
     ]
 
@@ -71,7 +72,7 @@ def generate_compliment():
     for word in range(n):
             try:
                     list_of_sentences = part1[word] + ' ' + part2[word] + ' ' + part3[word]
-                    list_of_sentences = list_of_sentences.capitalize() + '. '
+                    list_of_sentences += '.'
             except IndexError:
                     break
     return list_of_sentences
