8ad742969edde5d1ed3b438ca08be92ffff8d111
Dylan Hines <hitechdill@gmail.com>
2018-06-28 19:47:53 -0500

    Debug app key

--- oxford.py
+++ oxford.py
@@ -12,6 +12,8 @@ def define_word(word):
     url = 'https://od-api.oxforddictionaries.com:443/api/v1/entries/' + language + '/' + word_id.lower()
 
     r = requests.get(url, headers = {'app_id': app_id, 'app_key': app_key}).json()
+    print(app_id)
+    print(app_key)
 
     result = word.capitalize() + ": " + r['results']['lexicalEntries']['senses']['definitions'][0]
     return result
