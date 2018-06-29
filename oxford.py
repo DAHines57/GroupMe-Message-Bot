ce870682c0a460173835eb97bdce841b74dd61f3
Dylan Hines <hitechdill@gmail.com>
2018-06-28 19:30:44 -0500

    Why python dumb

--- oxford.py
+++ oxford.py
@@ -9,11 +9,9 @@ def define_word(word):
     language = 'en'
     word_id = word
 
-    url = 'https://od-api.oxforddictionaries.com:443/api/v1/entries/'
-    + language + '/' + word_id.lower()
+    url = 'https://od-api.oxforddictionaries.com:443/api/v1/entries/' + language + '/' + word_id.lower()
 
     r = requests.get(url, headers = {'app_id': app_id, 'app_key': app_key}).json()
 
-    result = word.capitalize() + ": " +
-    r['results']['lexicalEntries']['senses']['definitions'][0]
+    result = word.capitalize() + ": " + r['results']['lexicalEntries']['senses']['definitions'][0]
     return result
